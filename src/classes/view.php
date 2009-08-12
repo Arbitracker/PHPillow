fe88e015c3338bbfcd9133e9c3a0fdf2e31c042c
Robin Mehner <robin@coding-robin.de>
2009-08-12 09:31:07 +0000

    # fixed wiki links in view.php
    git-svn-id: svn://arbitracker.org/phpillow/trunk@141 c9640fd1-f2a0-481e-a8e5-009dc8289155

--- src/classes/view.php
+++ src/classes/view.php
@@ -172,7 +172,7 @@ abstract class phpillowView extends phpillowDocument
      *
      * Validates and transformed paased options to limit the view data, to fit
      * the specifications in the HTTP view API, documented at:
-     * http://www.couchdbwiki.com/index.php?title=HTTP_View_API#Querying_Options
+     * http://wiki.apache.org/couchdb/HTTP_view_API#Querying_Options
      * 
      * @param array $options 
      * @return string
@@ -247,7 +247,7 @@ abstract class phpillowView extends phpillowDocument
      * Query the specified view to get a set of results. You may optionally use
      * the view query options as additional paramters to limit the returns
      * values, specified at:
-     * http://www.couchdbwiki.com/index.php?title=HTTP_View_API#Querying_Options
+     * http://wiki.apache.org/couchdb/HTTP_view_API#Querying_Options
      * 
      * @param string $view 
      * @param array $options 
